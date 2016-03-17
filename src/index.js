05931a728fe69cc9e5cc169bd9eaf8ce5cf364b9
Chris Boone <cboone1337@gmail.com>
2016-03-16 20:09:51 -0500

    show nav items to user complete

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 'use strict';
 /* globals numWords, readTime */
 
- // loop through the .entry__info lists and add a new class for readtime
+const bodyEl = document.querySelector(`body`);
+const nav = document.createElement(`nav`);
+bodyEl.appendChild(nav);
+
+
+
 const entries = document.querySelectorAll(`.entry__info`);
 const entry = document.querySelectorAll(`.entry`);
 
@@ -14,4 +19,10 @@ for (let i = 0; i < entry.length; i++) {
   const timer = entry[i];
   const articleText = timer.querySelector(`.entry__content`).innerText;
   newEntry.innerText = `${readTime(articleText, 200)} mins`;
+
+  const title = timer.querySelector(`.entry__title`);
+  const link = document.createElement(`a`);
+  link.setAttribute(`href`, `#${timer.id}`);
+  link.innerText = title.innerText;
+  nav.appendChild(link);
 }
